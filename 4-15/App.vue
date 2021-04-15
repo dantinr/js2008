c4bbbc1cec59b1c0354396f8a2de535f9d56e7c2
dante <dantinr@gmail.com>
2021-04-15 15:57:04 +0800

    接口

---
+++ 4-15/App.vue
@@ -0,0 +1,21 @@
+<!--template 编写HTML-->
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      msg: 'vue-cli3开发单文件组件'
+    };
+  }
+};
+</script>
+<!--scoped表示注入，当前的样式只对当前的组件中的结果有效-->
+<style scoped>
+h3{
+  color:red;
+}
+</style>
